70649b0d2ad49fe6a7d656178d2c10b6e9889b91
Jon Jagger <jon@jaggersoft.com>
2017-03-25 17:41:51 +0000

    drop extraneous backslash in [C clang,assert]'s red_amber_green.rb lambda

--- C (clang)/assert/red_amber_green.rb
+++ C (clang)/assert/red_amber_green.rb
@@ -2,6 +2,6 @@
 lambda { |stdout,stderr,status|
   output = stdout + stderr
   return :red   if /(.*)Assertion(.*)failed./.match(output)
-  return :green if /(All|\\d+) tests passed/.match(output)
+  return :green if /(All|\d+) tests passed/.match(output)
   return :amber
 }
